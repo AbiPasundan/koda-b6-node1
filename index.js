7ebd58b1a0f868d55e2c3a5ca65c1ecca6dea068
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2026-01-02 10:40:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 import fs from "fs"
 
-const folderName = './music';
-const fileName = 'music.mp3';
+// const folderName = './music';
+// const fileName = 'music.mp3';
 
-fs.mkdir(folderName, (err) => {
-    if (err) {
-        console.error('Error creating directory:', err);
-    } else {
-        console.log('Directory created successfully!');
-        fs.writeFile(`${folderName}/${fileName}`, "content", err => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log("yeay");
-            }
-        });
-    }
+const artist = ["Iwan Fals", "Agnes Monica", "Sumbang"]
+
+const fileName = ['music.mp3']
+const folderName = ['./music']
+
+artist.forEach(element => {
+    // console.log(element);
+
+
+    fs.mkdir(element, (err) => {
+        if (err) {
+            console.error('Error creating directory:', err);
+        } else {
+            console.log('Directory created successfully!');
+            fs.writeFile(`${element}/${element}.mp3`, "content", err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("yeay");
+                }
+            });
+        }
+    });
+
+
 });
+
+
+
+// fs.mkdir(folderName, (err) => {
+//     if (err) {
+//         console.error('Error creating directory:', err);
+//     } else {
+//         console.log('Directory created successfully!');
+//         fs.writeFile(`${folderName}/${fileName}`, "content", err => {
+//             if (err) {
+//                 console.error(err);
+//             } else {
+//                 console.log("yeay");
+//             }
+//         });
+//     }
+// });
 
